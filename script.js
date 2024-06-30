ca82b1a0c3116bcc679d0b5d68ae10b1fa27e5f4
void <codestrife00@gmail.com>
2024-06-30 14:22:46 -0500

    finish core game loop

--- script.js
+++ script.js
@@ -5,18 +5,14 @@ var operator;
 var solution;
 
 var score = 0;
-var timer;
-
-var solveSpeed;
-
-var gameMode;
-var container = document.getElementsByClassName("container")[0]
-var gameOverScreen;
-var restartButton;
-let countDown
+var gameMode; 
+var survivalTime = 1.5;
+var sprintTime = 60;
+var countDown;
+var container = document.getElementsByClassName("container")[0];
 
 function getOperator(){
-    randomNumber = Math.random()
+    let randomNumber = Math.random()
     if (score < 15) {
         operator = '+';
     } else if (score >= 15 && score <= 34) {
@@ -24,7 +20,7 @@ function getOperator(){
             operator = '+';
         } else {
             operator = '-';
-        }
+        };
     } else if (score >= 35 && score <=60) {
         if (randomNumber < 0.33) {
             operator = '+';
@@ -32,40 +28,41 @@ function getOperator(){
             operator = '-';
         } else {
             operator = '*';
-        }
+        };
     }
     return operator;
 };
 
-// returns 1 (inclusive) to max (exclusive)
 function getRandomInt(){
+    // returns 1 (inclusive) to max (exclusive)
+    let term = 0;
     if (operator == '+') {
         if (score <= 4 ) {
-            var term = Math.floor(Math.random() * (5 - 1) + 1);
+            term = Math.floor(Math.random() * (5 - 1) + 1);
         } else if (score >= 5 && score <= 14) {
-            var term = Math.floor(Math.random() * (10 - 1) + 1);
+            term = Math.floor(Math.random() * (10 - 1) + 1);
         } else if (score >= 15 && score <= 29) {
-            var term = Math.floor(Math.random() * (20 - 1) + 1);
+            term = Math.floor(Math.random() * (20 - 1) + 1);
         } else {
-            var term = Math.floor(Math.random() * (40 - 1) + 1);
-        }
+            term = Math.floor(Math.random() * (40 - 1) + 1);
+        };
     } else if (operator == '-') {
         if (score <= 20 ) {
-            var term = Math.floor(Math.random() * (10 - 1) + 1);
+            term = Math.floor(Math.random() * (10 - 1) + 1);
         } else if (score >= 21 && score <= 30) {
-            var term = Math.floor(Math.random() * (20 - 1) + 1);
+            term = Math.floor(Math.random() * (20 - 1) + 1);
         } else {
-            var term = Math.floor(Math.random() * (40 - 1) + 1);
-        } 
+            term = Math.floor(Math.random() * (40 - 1) + 1);
+        };
     } else if (operator == '*') {
         if (score <= 45 ) {
-            var term = Math.floor(Math.random() * (9 - 1) + 1);
+            term = Math.floor(Math.random() * (9 - 1) + 1);
         } else if (score >= 46 && score <= 60) {
-            var term = Math.floor(Math.random() * (13 - 1) + 1);
+            term = Math.floor(Math.random() * (13 - 1) + 1);
         } else {
-            var term = Math.floor(Math.random() * (15 - 1) + 1);
-        } 
-    }
+            term = Math.floor(Math.random() * (15 - 1) + 1);
+        };
+    };
     return term;
 };
 
@@ -76,202 +73,222 @@ function calculateSolution(termA, termB, operator){
         return (termA - termB);
     }  else if (operator == '*') {
         return (termA * termB);
-    }  
-}
+    };
+};
 
 function generateEquation(){
     operator = getOperator();
     termA = getRandomInt();
     termB = getRandomInt();
-    solution = calculateSolution(termA, termB, operator)
-    console.log(termA + " " + operator + " " + termB)
-    return termA + " " + operator + " " + termB
-    
-}
+    solution = calculateSolution(termA, termB, operator);
+    // console.log(termA + " " + operator + " " + termB)
+    return termA + " " + operator + " " + termB;
+};
 
 function assignSoutions(){
-    var solutionA = document.getElementById("solutionA");
-    var solutionB = document.getElementById("solutionB");
+    let solutionA = document.getElementById("solutionA");
+    let solutionB = document.getElementById("solutionB");
 
     if (Math.random() < 0.5){
-        solutionA.innerText = solution
+        solutionA.innerText = solution;
         if (Math.random() < 0.5){
-            solutionB.innerText = solution - 1
+            solutionB.innerText = solution - 1;
         } else {
-            solutionB.innerText = solution + 1
-        }
+            solutionB.innerText = solution + 1;
+        };
     } else {
-        solutionB.innerText = solution
+        solutionB.innerText = solution;
         if (Math.random() < 0.5){
-            solutionA.innerText = solution - 1
+            solutionA.innerText = solution - 1;
         } else {
-            solutionA.innerText = solution + 1
-        }
-    }
-}
+            solutionA.innerText = solution + 1;
+        };
+    };
+};
 
-// --------------------
-// click event handling
-// --------------------
-document.addEventListener( "click", clickHandler );
+function buildMenuScreen() {
+    let gameOverScreen = document.getElementById("gameOverScreen");
+    gameOverScreen.remove();
 
-function clickHandler(event){
-    var element = event.target;
-
-    if (element.tagName == "BUTTON" && element.id == "survival-btn"){
-        gameMode = "survival"
-        buildGameScreen()
-        updateGameScreen()
-        startTimer(gameMode)
-    } else if (element.tagName == "BUTTON" && element.id == "sprint-btn"){
-        gameMode = "sprint"
-        buildGameScreen()
-        updateGameScreen()
-        startTimer(gameMode)
-    } else if (element.tagName == "BUTTON" && element.id == "solutionA"){
-        if (solutionA.innerText == solution) {
-            score++
-            updateGameScreen()
-            if (gameMode == "survival") {
-                clearInterval(countDown)
-                startTimer(gameMode)
-            }
-        } else {
-            clearInterval(countDown)
-            buildGameOverScreen()
-        }
-    } else if (element.tagName == "BUTTON" && element.id == "solutionB"){
-        if (solutionB.innerText == solution) {
-            score++
-            updateGameScreen()
-            if (gameMode == "survival") {
-                clearInterval(countDown)
-                startTimer(gameMode)
-            }
-        } else {
-            clearInterval(countDown)
-            buildGameOverScreen()
-        }
-    } else if (element.tagName == "BUTTON" && element.id == "restart"){
-        buildGameScreen()
-        updateGameScreen()
-        startTimer(gameMode)
-    }
-}
-
-function buildGameScreen(){
-
-    var menuScreen = document.getElementById("menuScreen");
-    var gameOverScreen = document.getElementById("gameOverScreen");
-    if (menuScreen){
-        menuScreen.remove()
-    } else {
-        gameOverScreen.remove()
-    }
+    let newElement = document.createElement("div");
+    newElement.className = "container";
+    newElement.id = "menuScreen";
+    let tempA = newElement;
     
-    var newElement = document.createElement("div")
-    newElement.className = "container"
-    newElement.id = "gameScreen"
+    newElement = document.createElement("button");
+    newElement.id = "survival-btn";
+    newElement.innerText = "Survival";
+    tempA.appendChild(newElement);
+
+    newElement = document.createElement("button");
+    newElement.id = "sprint-btn";
+    newElement.innerText = "Sprint";
+    tempA.appendChild(newElement);
+
+    container.appendChild(tempA);
+};
+
+function buildGameScreen() {
+    let menuScreen = document.getElementById("menuScreen");
+    menuScreen.remove();
     
-    var tempA = newElement
+    let newElement = document.createElement("div");
+    newElement.className = "container";
+    newElement.id = "gameScreen";
+    let tempA = newElement;
 
-    newElement = document.createElement("div")
-    newElement.className = "progress"
+    newElement = document.createElement("div");
+    newElement.className = "progress";
+    let tempB = newElement;
 
-    var tempB = newElement
+    newElement = document.createElement("div");
+    newElement.className = "progress-inner";
+    tempB.appendChild(newElement);
+    tempA.appendChild(tempB);
 
-    newElement = document.createElement("div")
-    newElement.className = "progress-inner"
+    newElement = document.createElement("p");
+    newElement.id = "equationText";
+    tempA.appendChild(newElement);
 
-    tempB.appendChild(newElement)
-    tempA.appendChild(tempB)
+    newElement = document.createElement("div");
+    newElement.id = "answer-buttons";
+    tempB = newElement;
 
-    newElement = document.createElement("p")
-    newElement.id = "equationText"
-    tempA.appendChild(newElement)
+    newElement = document.createElement("button");
+    newElement.id = "solutionA";
+    tempB.appendChild(newElement);
 
-    newElement = document.createElement("div")
-    newElement.id = "answer-buttons"
+    newElement = document.createElement("button");
+    newElement.id = "solutionB";
+    tempB.appendChild(newElement);
+    tempA.appendChild(tempB);
 
-    tempB = newElement
+    container.appendChild(tempA);
+};
 
-    newElement = document.createElement("button")
-    newElement.id = "solutionA"
-    tempB.appendChild(newElement)
+function buildGameOverScreen() {
+    let gameScreen = document.getElementById("gameScreen");
+    gameScreen.remove();
 
-    newElement = document.createElement("button")
-    newElement.id = "solutionB"
-    tempB.appendChild(newElement)
+    let newElement = document.createElement("div");
+    newElement.className = "container";
+    newElement.id = "gameOverScreen";
+    let tempA = newElement;
 
-    tempA.appendChild(tempB)
+    newElement = document.createElement("p");
+    newElement.id = "gameOverText";
+    newElement.innerText = "Game Over";
+    tempA.appendChild(newElement);
 
-    container.appendChild(tempA)
-}
+    newElement = document.createElement("p");
+    newElement.id = "scoreText";
+    newElement.innerText = "Score: ";
+    let tempB = newElement;
 
-function updateGameScreen(){
-    var equation = document.getElementById("equationText");
-    equation.innerText = generateEquation()
-    assignSoutions()
-}
-
-function buildGameOverScreen(){
-    var gameScreen = document.getElementById("gameScreen")
-    gameScreen.remove()
-
-    var newElement = document.createElement("div")
-    newElement.className = "container"
-    newElement.id = "gameOverScreen"
+    newElement = document.createElement("span");
+    newElement.id = "scoreInt";
+    newElement.innerText = score;
+    tempB.appendChild(newElement);
+    tempA.appendChild(tempB);
     
-    var tempA = newElement
+    newElement = document.createElement("button");
+    newElement.id = "restart";
+    newElement.innerText = "Play Again?";
+    tempA.appendChild(newElement);
 
-    newElement = document.createElement("p")
-    newElement.id = "gameOverText"
-    newElement.innerText = "GAME OVER LOSER"
-    tempA.appendChild(newElement)
+    container.appendChild(tempA);
+};
 
-    newElement = document.createElement("p")
-    newElement.id = "scoreText"
-    newElement.innerText = "you scored: " + score
-    tempA.appendChild(newElement)
-    
-    newElement = document.createElement("button")
-    newElement.id = "restart"
-    newElement.innerText = "play again?"
-    tempA.appendChild(newElement)
-
-    container.appendChild(tempA)
-}
+function updateGameScreen() {
+    let equation = document.getElementById("equationText");
+    equation.innerText = generateEquation();
+    assignSoutions();
+};
 
 function startTimer(gameMode) {
-
-    var progressBar = document.getElementsByClassName("progress-inner")[0]
+    let progressBar = document.getElementsByClassName("progress-inner")[0]
 
     if (gameMode == "survival") {
-        let interval = 2;
+        let interval = survivalTime;
         countDown = setInterval(() => {
             interval-= 0.005;
-            let progressWidth = (interval / 2) * 100
+            let progressWidth = (interval / 2) * 100;
             if (interval > 0) {
-                progressBar.style.width = progressWidth/2 + "%"
+                progressBar.style.width = progressWidth/2 + "%";
             } else {
-                clearInterval(countDown)
-                progressBar.style.width = "0"
-                buildGameOverScreen()
-            }
+                clearInterval(countDown);
+                progressBar.style.width = "0";
+                buildGameOverScreen();
+            };
         }, 5);
     } else if (gameMode == "sprint") {
-        var interval = 60;
+        let interval = sprintTime;
         countDown = setInterval(() => {
-            interval--;
-            let progressWidth = (interval / 60) * 100;
-            if (interval > 0) {
-                progressBar.style.width = progressWidth + "%"
+            sprintTime--;
+            let progressWidth = (sprintTime / 60) * 100;
+            if (sprintTime > 0) {
+                progressBar.style.width = progressWidth + "%";
             } else {
-                clearInterval(countDown)
-                progressBar.style.width = "0"
-                buildGameOverScreen()
-            }
+                clearInterval(countDown);
+                progressBar.style.width = "0";
+                buildGameOverScreen();
+            };
         }, 1000);
     }
-}
+};
 
+function inputHandler(event){
+    let element = event.target; 
+
+    if (event.type === "click") {
+        if (element.tagName == "BUTTON" && element.id == "survival-btn"){
+            gameMode = "survival";
+            buildGameScreen();
+            updateGameScreen();
+            startTimer(gameMode);
+        } else if (element.tagName == "BUTTON" && element.id == "sprint-btn"){
+            gameMode = "sprint";
+            buildGameScreen();
+            updateGameScreen();
+            startTimer(gameMode);
+        } else if (element.tagName == "BUTTON" && element.id == "solutionA"){
+            if (solutionA.innerText == solution) {
+                score++;
+                updateGameScreen();
+                if (gameMode == "survival") {
+                    clearInterval(countDown);
+                    startTimer(gameMode);
+                };
+            } else {
+                clearInterval(countDown);
+                buildGameOverScreen();
+                gameMode = "";
+            };
+        } else if (element.tagName == "BUTTON" && element.id == "solutionB"){
+            if (solutionB.innerText == solution) {
+                score++;
+                updateGameScreen();
+                if (gameMode == "survival") {
+                    clearInterval(countDown);
+                    startTimer(gameMode);
+                };
+            } else {
+                clearInterval(countDown);
+                buildGameOverScreen();
+                gameMode = "";
+            }
+        } else if (element.tagName == "BUTTON" && element.id == "restart"){
+            buildMenuScreen();
+        }
+    } else if (event.type === "keydown" && gameMode) { 
+        if (event.key === "ArrowLeft") {
+          document.getElementById("solutionA").click();
+        } else if (event.key === "ArrowRight") {
+          document.getElementById("solutionB").click();
+        };
+    };
+};
+
+// Input handling
+document.addEventListener( "click", inputHandler );
+document.addEventListener("keydown", inputHandler);
